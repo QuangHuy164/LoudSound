d256dc9783e21826597c93ff1b8aae0a47e36af9
Huy Nguyen <nghuy164@gmail.com>
2025-10-01 02:28:19 +0300

    update

---
+++ data/songs.js
@@ -0,0 +1,16 @@
+const songs = [
+    {
+        id: 1,
+        name: 'Nhu vay nhe',
+        artist: 'Khac Viet',
+        path: '',
+        image: ''
+    },
+    {
+        id: 2,
+        name: 'Mixtape 2h',
+        artist: '',
+        path: '',
+        image: ''
+    }
+]
